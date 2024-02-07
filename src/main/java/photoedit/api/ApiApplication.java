f35e1fcd0ccc561ef6bd1eac4b641f1821e7fec0
Bartek <01187030@pw.edu.pl>
2024-02-07 18:33:44 +0100

    adds ints

--- src/main/java/photoedit/api/ApiApplication.java
+++ src/main/java/photoedit/api/ApiApplication.java
@@ -2,13 +2,23 @@ package photoedit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 		//change
+	}
+	@GetMapping("/add/{input1}/{input2}")
+	public int addIntegers(@PathVariable int input1, @PathVariable int input2) {
+		return input1 + input2;
 	}
 
 }
